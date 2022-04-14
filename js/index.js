323892f602772b41d089b07d9bac7941f8ea468d
[Leonard Canastra] <[canastraleonard123@gmail.com]>
2022-04-13 23:18:24 -0500

    yes

--- js/index.js
+++ js/index.js
@@ -1,24 +1,33 @@
+
 document.querySelector('.get').addEventListener('click', getAnime);
 
 let data;
+
+//API request to get anime, will put anime into a card in DOM
 function getAnime() {
     fetch("https://api.jikan.moe/v4/random/anime")
         .then(res => res.json()) // parse response as JSON
         .then(_data => {
             data = _data;
             console.log(data);
-            if (data.data.score < 7 || data.data.score == null) {
-                getAnime()
-            }
             document.querySelector('h2').innerText = data.data.title;
             document.querySelector('img').src = data.data.images.jpg.image_url;
             document.querySelector('h3').innerText = data.data.score;
+            document.querySelector('p').innerText = data.data.synopsis;
+
 
         })
         .catch(err => {
             console.log(`error ${err}`)
         });
 
-
 }
 
+
+// //function to call on
+// function addAnime() {
+//     let i = 0;
+//     while (i <= 3) {
+//         getAnime();
+//     }
+// }
