e2927b6c59d30c85070ce95ed3b296c9373b94b1
[Leonard Canastra] <[canastraleonard123@gmail.com]>
2022-04-27 18:44:43 -0500

    Worked on JS to append new cards

--- js/main.js
+++ js/main.js
@@ -1,22 +1,32 @@
 
 
-document.querySelector('.get').addEventListener('click', getAnime);
+document.querySelector('.get').addEventListener('click', fetchAnime);
 
 let data;
-
+let i = 0;
 //API request to get anime, will put anime into a card in DOM
-function getAnime() {
+function fetchAnime() {
     fetch("https://api.jikan.moe/v4/random/anime")
         .then(res => res.json()) // parse response as JSON
         .then(_data => {
             data = _data;
             console.log(data);
-            document.querySelector('h2').innerText = data.data.title;
-            document.querySelector('img').src = data.data.images.jpg.image_url;
-            document.querySelector('h3').innerText = data.data.score;
-            document.querySelector('p').innerText = data.data.synopsis;
+            const temp = document.createElement('div');
+            temp.className = 'card' + i
+            const tempImg = document.createElement('img');
+            tempImg.className = 'newImg'
+            tempImg.src = data.data.images.jpg.image_url;
+            document.getElementsByClassName('container')[0].appendChild(temp);
+            document.getElementsByClassName('card' + i)[0].appendChild(tempImg);
+            i++;
 
 
+
+
+            // document.querySelector('h2').innerText = data.data.title;
+            // document.querySelector('img').src = data.data.images.jpg.image_url;
+            // document.querySelector('h3').innerText = data.data.score;
+            // document.querySelector('p').innerText = data.data.synopsis;
         })
         .catch(err => {
             console.log(`error ${err}`)
@@ -25,10 +35,3 @@ function getAnime() {
 }
 
 
-// //function to call on
-// function addAnime() {
-//     let i = 0;
-//     while (i <= 3) {
-//         getAnime();
-//     }
-// }
